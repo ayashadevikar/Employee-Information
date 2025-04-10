a312c7f07fea69379bca5f3e8161a005dadba962
ayashadevikar <ayashadevikar8@gmail.com>
2025-04-10 17:28:10 +0530

    backend api added

--- fullstackcrud.client/vite.config.js
+++ fullstackcrud.client/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
@@ -7,4 +8,6 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
+
+  plugins: [react()],
 });
